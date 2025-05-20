85062a46325e787bfaccd621b21a04e4a9e2cd27
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-05-20 11:35:59 +0000

    kyc

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
-apt-get update && apt-get install -y tesseract-ocr libgl1
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install -y \
+  build-essential \
+  cmake \
+  libopenblas-dev \
+  liblapack-dev \
+  libx11-dev \
+  libgtk-3-dev \
+  tesseract-ocr \
+  libgl1
+
+pip install --upgrade pip
+pip install -r requirements.txt
